fd74f12da107158d9bbdaf216e4656bd995badd3
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2018-10-01 15:09:46 +0900

    Add files via upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour {
+
+    public UnityEvent onReset;
+
+    public static GameManager instance;
+
+    public GameObject readyPannel;
+
+    public Text scoreText;
+
+    public Text bestScoreText;
+
+    public Text messageText;
+
+    public bool isRoundActive = false;
+
+    private int score = 0;
+
+    public ShooterRotator shooterRotator;
+
+    public CamFollow cam;
+
+	// Use this for initialization
+	void Awake () {
+        instance = this;
+        UpdateUI();
+	}
+
+    void Start()
+    {
+        StartCoroutine("RoundRoutine");
+    }
+
+
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+        UpdateBestScore();
+        UpdateUI();
+    }
+	
+    void UpdateBestScore()
+    {
+        if (GetBsetScore() < score)
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+        }
+    }
+
+    int GetBsetScore()
+    {
+        int bestScore = PlayerPrefs.GetInt("BestScore");
+        return bestScore;
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+        bestScoreText.text = "Best Score: " + GetBsetScore();
+    }
+
+    public void OnBallDestroy()
+    {
+        UpdateUI();
+        isRoundActive = false;
+    }
+
+    public void Reset()
+    {
+        score = 0;
+        UpdateUI();
+
+        //라운드를 다시 처음부터 시작
+        StartCoroutine("RoundRoutine");
+    }
+
+    IEnumerator RoundRoutine()
+    {
+        //READY
+        onReset.Invoke();
+
+        readyPannel.SetActive(true);
+        cam.SetTarget(shooterRotator.transform,CamFollow.State.Idle);
+        shooterRotator.enabled = false;
+
+        isRoundActive = false;
+
+        messageText.text = "Ready...";
+
+        yield return new WaitForSeconds(3f);
+
+        //PLAY
+        isRoundActive = true;
+        readyPannel.SetActive(false);
+        shooterRotator.enabled = true;
+
+        cam.SetTarget(shooterRotator.transform, CamFollow.State.Ready);
+
+        while(isRoundActive == true)
+        {
+            yield return null;
+        }
+
+        //END
+
+        readyPannel.SetActive(true);
+        shooterRotator.enabled = false;
+
+        messageText.text = "Wait For Next Round...";
+        yield return new WaitForSeconds(3f);
+
+        Reset();
+    }
+}
